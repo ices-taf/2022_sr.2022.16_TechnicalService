83deb07a2f3d88bb0c8797918fe918d17b784a54
Colin Millar <colin.millar@ices.dk>
2022-10-14 11:21:39 +0200

    added banana hole filter to data prep

--- data_prep.R
+++ data_prep.R
@@ -2,6 +2,10 @@ library(icesTAF)
 library(data.table)
 library(dplyr)
 library(icesVMS)
+library(sf)
+library(ggplot2)
+
+source("utilities.R")
 
 mkdir("data")
 
@@ -27,10 +31,37 @@ vms_all <-
 
 dim(vms_all)
 
+# load shape
+bannana <- read_sf("bootstrap/data/shapes/area.csv", )
+st_crs(bannana) <- 4326
+
+c_squares <-
+  tibble(
+    c_square = unique(vms_all[["c_square"]])
+  ) %>%
+  vms_add_spatial()
+
+
+ggplot() +
+  geom_sf(data = c_squares) +
+  geom_sf(data = bannana, fill = NA)
+
+# spatial filter
+int <- st_contains(bannana, c_squares, sparse = FALSE)
+dim(int)
+any(int)
+
+c_squares_bananna <- c_squares[which(int),]
+
+ggplot() +
+  geom_sf(data = c_squares_bananna) +
+  geom_sf(data = bannana, fill = NA)
+
+
 # join and filter
 vms <-
   vms_all %>%
-  filter(!is.na(c_square)) %>% 
+  filter(c_square %in% c_squares_bananna$c_square) %>% 
   left_join(
     metier_lookup,
     by = c(LE_MET_level6 = "leMetLevel6")
